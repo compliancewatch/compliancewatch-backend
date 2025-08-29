7596955ee91887e972e894265a69c65eb8477632
ComplianceWatch.Global <compliancewatch.global@gmail.com>
2025-08-29 23:20:14 +0300

    Update unctad.js

--- src/scrapers/unctad.js
+++ src/scrapers/unctad.js
@@ -1,10 +1,10 @@
 import { runGenericScraper } from './generic-scraper.js';
 import { REGULATORY_TARGETS } from '../../config/targets.js';
 
-const target = REGULATORY_TARGETS.find(t => t.scraper === 'unctad');
+const target = REGULATORY_TARGETS.find(t => t.scraper === 'un-security-council');
 
-export async function runUNCTADScraper() {
+export async function runUNSecurityCouncilScraper() {
   return runGenericScraper(target);
 }
 
-export default runUNCTADScraper;
+export default runUNSecurityCouncilScraper;
